1b08b136882d4fe75274338450712c14885a8047
Chris <chris@mbp-de-chris.home>
2018-12-18 22:30:08 +0100

    v2.0.12

--- kupiki_updater.sh
+++ kupiki_updater.sh
@@ -12,7 +12,11 @@ LOGNAME="kupiki_updater.log"
 LOGPATH="/var/log/"
 KUPIKI_SCRIPT_ARCHIVE="https://raw.githubusercontent.com/pihomeserver/Kupiki-Hotspot-Script/master/pihotspot.sh"
 
-declare -a KUPIKI_UPDATES=("2.0.1" "2.0.2" "2.0.3" "2.0.4" "2.0.5" "2.0.6" "2.0.7" "2.0.8" "2.0.9" "2.0.10" "2.0.11")
+declare -a KUPIKI_UPDATES=("2.0.1" "2.0.2" "2.0.3" "2.0.4" "2.0.5" "2.0.6" "2.0.7" "2.0.8" "2.0.9" "2.0.10" "2.0.11" "2.0.12")
+
+upgrade_2.0.12() {
+  :
+}
 
 upgrade_2.0.11() {
   grep HS_UAMDOMAINS /etc/chilli/config
